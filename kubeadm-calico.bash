3e9433424b65ead777c373affc1bbf817dbfbf1d
Philip Lombardi <plombardi@datawire.io>
2017-07-22 18:39:05 -0400

    Remove RBAC for now

--- kubeadm-calico.bash
+++ kubeadm-calico.bash
@@ -26,7 +26,6 @@ rm /tmp/kubeadm.yaml
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 # Calico
-kubectl apply -f http://docs.projectcalico.org/v2.3/getting-started/kubernetes/installation/rbac.yaml
 kubectl apply -f http://docs.projectcalico.org/v2.3/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
 
 # Allow containers to run on the Kubernetes Master
